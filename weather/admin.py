465f7f0aa8c05cbfa96d8bee3a8053d600144f66
Tommy Gebhardt <tommygebhardt@gmail.com>
2014-12-09 15:44:56 -0700

    deleted autosave files and corrected detail.html

--- weather/admin.py
+++ weather/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from weather.models import node
+from weather.models import node, report
 # Register your models here.
 
 admin.site.register(node)
+admin.site.register(report)
